939f63f364a7aafb12f1fada33526cf951a55ba2
97gkswn <97gkswn@naver.com>
2022-08-15 15:53:44 +0900

    model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin
 
-# Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(blank=False)
+    password = models.CharField(blank=False)
+    profile_image = models.ImageField(blank=False)
+    username = models.CharField(blank=False)
+    univ = models.CharField(blank=False)
--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from auth.models import User
 
-# Create your models here.
+class Plan(models.Model):
+    email = models.ForeignKey(User)
+    present_time = models.DateTimeField(auto_now_add=True)
+    isDone = models.BooleanField()
+    promise_time = models.IntegerField()
+    title = models.CharField(blank=False)
+    place_name = models.CharField()
+    place_id = models.IntegerField()
+    max_count=models.IntegerField()
+    reward=models.CharField()
+    name=models.IntegerField()
+    category=models.CharField()
+    
