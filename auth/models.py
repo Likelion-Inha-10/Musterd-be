b745ce2e82efda3050c1bf9fbf304f02e44137e6
namesnames <103047410+namesnames@users.noreply.github.com>
2022-08-15 18:58:56 +0900

    Delete auth directory

--- auth/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin
-
-class User(AbstractBaseUser,PermissionsMixin):
-    email = models.EmailField(blank=False)
-    password = models.CharField(blank=False)
-    profile_image = models.ImageField(blank=False)
-    username = models.CharField(blank=False)
-    univ = models.CharField(blank=False)
